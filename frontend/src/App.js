8d82534f17ea4c79bdfd752e38423d7e86a14935
newbi-lumutan <irmawidantari@gmail.com>
2026-03-28 00:48:06 +0700

    Create App.js component for VPS Panel

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>VPS Panel</h1>
+      <p>Your management panel for Virtual Private Servers.</p>
+    </div>
+  );
+};
+
+export default App;
